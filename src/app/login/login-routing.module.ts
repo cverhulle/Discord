b5a44fdedc273ac3c3f3e24553b065e64a7c2198
cverhulle <corentin.verhulle@gmail.com>
2025-02-12 17:02:50 +0100

    Création du component pour s'inscrire et des routes.

--- src/app/login/login-routing.module.ts
+++ src/app/login/login-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginHomepageComponent } from './components/login-homepage/login-homepage.component';
+import { RegisterComponent } from './components/register/register.component';
 
 const routes: Routes = [
+  { path: 'register', component: RegisterComponent },
   { path: '', component: LoginHomepageComponent }
 ];
 
