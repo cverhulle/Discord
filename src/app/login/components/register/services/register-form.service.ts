2a40a40ae7a2b3152dafd1d6b9b962d8468f03ab
cverhulle <corentin.verhulle@gmail.com>
2025-02-17 15:22:35 +0100

    Ajout de la méthode emailExists pour vérifier si l'email est déjà utilisé dans le service.

--- src/app/login/components/register/services/register-form.service.ts
+++ src/app/login/components/register/services/register-form.service.ts
@@ -3,6 +3,7 @@ import { catchError, map, Observable, of } from "rxjs";
 import { environment } from "../../../../../environments/environment.development";
 import { HttpClient } from "@angular/common/http";
 import { RegisterForm } from "../models/register-form.model";
+import { FormControl } from "@angular/forms";
 
 @Injectable()
 
@@ -11,6 +12,13 @@ export class RegisterFormService {
 
     saveUserInfo(formValue: RegisterForm): Observable<boolean> {
         return this.http.post(`${environment.apiUrl}/users/register`, formValue).pipe(
+            map( () => true),
+            catchError(() => of(false))
+        );
+    }
+
+    emailExists(formControl: FormControl): Observable<boolean>{
+        return this.http.post(`${environment.apiUrl}/users/register`, formControl).pipe(
             map( () => true),
             catchError(() => of(false))
         );
