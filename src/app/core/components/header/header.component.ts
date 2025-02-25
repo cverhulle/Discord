135bf996da7792d7fe5361b8adb44c4b54ce35be
cverhulle <corentin.verhulle@gmail.com>
2025-02-25 11:39:43 +0100

    Suppression de l'id dans la route du profil. Suppression de l'userId dans le ts de header component.

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -14,21 +14,13 @@ import { TokenService } from '../../../interceptors/services/auth.service';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent implements OnInit{
-  userId!: string| null
-
+  
   constructor(private tokenService : TokenService) {}
 
   ngOnInit(): void {
-    this.userId = this.tokenService.getUserId()
+  
   }
 
-  onChargeUserId(): void {
-    if(this.tokenService.isLogged()) {
-      this.userId = this.tokenService.getUserId()
-    } else {
-      this.userId = '67b5d75126a7c6922f7db331'
-    }
-
-  }
+  
 
 }
