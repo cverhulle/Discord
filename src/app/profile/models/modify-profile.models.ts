3a8c590c1ee19cfaec9cc5ce62977fbca59a6601
cverhulle <corentin.verhulle@gmail.com>
2025-02-26 15:14:55 +0100

    Création de ModifyProfileForm qui correspond à RegisterForm sans le mot de passe.

---
+++ src/app/profile/models/modify-profile.models.ts
@@ -0,0 +1,16 @@
+export class ModifyProfileForm {
+    personalInfo!: {
+      firstName: string,
+      lastName: string,
+    }
+    emailInfo!: {
+      email: string,
+      confirmEmail: string
+    }
+    
+    username!: string
+     
+    image!: string
+  }
+  
+  
