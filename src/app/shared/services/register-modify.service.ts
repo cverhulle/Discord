00a037b487da70c3620bf549ffd374ef6b1ae51f
cverhulle <corentin.verhulle@gmail.com>
2025-02-27 18:08:22 +0100

    Création des Observables pour les erreurs d'emaile d'username.

--- src/app/shared/services/register-modify.service.ts
+++ src/app/shared/services/register-modify.service.ts
@@ -11,7 +11,29 @@ export class RegisterModifyService{
     private loadingSuject = new BehaviorSubject<boolean>(false)
     loading$ = this.loadingSuject.asObservable()
 
+    // Variable pour l'erreur d'email
+    private errorEmailSuject = new BehaviorSubject<boolean>(false)
+    errorEmail$ = this.errorEmailSuject.asObservable()
+
+    // Variable pour l'erreur d'username
+    private errorUsernameSuject = new BehaviorSubject<boolean>(false)
+    errorUsername$ = this.errorUsernameSuject.asObservable()
+
+
     setLoading(loading: boolean) {
         this.loadingSuject.next(loading)
     }
+
+    setErrorEmail(errorEmail: boolean) {
+        this.errorEmailSuject.next(errorEmail)
+    }
+
+    setErrorUsername(errorUsername: boolean) {
+        this.errorUsernameSuject.next(errorUsername)
+    }
+
+
+    
+
+
 }
