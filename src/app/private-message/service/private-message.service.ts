4fb75a6650b60cb7da04a9d55d2da1be5ae61048
cverhulle <corentin.verhulle@gmail.com>
2025-03-04 17:05:17 +0100

    Création de la méthode searchQueryUsers pour trouver lancer la recherche dans le back end.

--- src/app/private-message/service/private-message.service.ts
+++ src/app/private-message/service/private-message.service.ts
@@ -1,7 +1,15 @@
+import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { usernameImage } from "../models/username-image.models";
+import { environment } from "../../../environments/environment.development";
 
 @Injectable()
 
 export class PrivateMessageService {
-    
+    constructor(private http: HttpClient) {}
+
+    searchQueryUsers(query: string) : Observable<usernameImage[]> {
+        return this.http.get<usernameImage[]>(`${environment.apiUrl}/private-message/queryUsers?search=${query}`)
+    }
 }
