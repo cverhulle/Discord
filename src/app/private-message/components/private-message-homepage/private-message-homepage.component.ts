1534bc6b637a734cee9b1dbd3293f6e451e132cb
cverhulle <corentin.verhulle@gmail.com>
2025-03-04 11:42:05 +0100

    Amélioration de l'aspect esthétique du système de recherhce.

--- src/app/private-message/components/private-message-homepage/private-message-homepage.component.ts
+++ src/app/private-message/components/private-message-homepage/private-message-homepage.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { FormsModule, NgModel } from '@angular/forms';
+import { SharedModule } from '../../../shared/shared.module';
 
 @Component({
   selector: 'app-private-message-homepage',
   imports: [
     FormsModule,
+    SharedModule
   ],
   templateUrl: './private-message-homepage.component.html',
   styleUrl: './private-message-homepage.component.scss'
