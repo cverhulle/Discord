a0153c1e34b5a29f88b21b9490ba05c402c70399
cverhulle <corentin.verhulle@gmail.com>
2025-03-04 11:51:49 +0100

    Création de la variable users. Initialisation des deux variables.

--- src/app/private-message/components/private-message-homepage/private-message-homepage.component.ts
+++ src/app/private-message/components/private-message-homepage/private-message-homepage.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormsModule, NgModel } from '@angular/forms';
 import { SharedModule } from '../../../shared/shared.module';
+import { usernameImage } from '../../models/username-image.models';
 
 @Component({
   selector: 'app-private-message-homepage',
@@ -15,4 +16,14 @@ export class PrivateMessageHomepageComponent {
   // Contient la requete de l'utilisateur
   searchQuery!: string;
   
+
+  // Variable pour contenir la liste des utilisateurs correspondant à la requete
+  users!: usernameImage[];
+
+  constructor() {
+    this.searchQuery = '',
+    this.users = []
+  }
+
+
 }
