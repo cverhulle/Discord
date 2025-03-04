e65dd53313c2a1bb43c5d922aa9af7a7f4c4c00b
cverhulle <corentin.verhulle@gmail.com>
2025-03-04 16:55:20 +0100

    Introduction d'un Subject pour lancer la recherche après 1 s sans mouvement dans le formulaire. Modication de la méthode appelée dans le template : onInputChange()

--- src/app/private-message/components/private-message-homepage/private-message-homepage.component.ts
+++ src/app/private-message/components/private-message-homepage/private-message-homepage.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormsModule, NgModel } from '@angular/forms';
 import { SharedModule } from '../../../shared/shared.module';
 import { usernameImage } from '../../models/username-image.models';
+import { debounce, debounceTime, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-private-message-homepage',
@@ -16,17 +17,32 @@ export class PrivateMessageHomepageComponent {
   // Contient la requete de l'utilisateur
   searchQuery!: string;
   
-
   // Variable pour contenir la liste des utilisateurs correspondant à la requete
   users!: usernameImage[];
 
+  // On crée un Subject pour réagir aux changements sur le formulaire
+  private searchSubject: Subject<string> = new Subject();
+
   constructor() {
-    this.searchQuery = '',
-    this.users = []
+    this.searchQuery = '';
+    this.users = [];
+
+    this.searchSubject.pipe(
+      debounceTime(1000)
+    ).subscribe(query => {
+      this.onSearch(query)
+    })
   }
 
-  onSearch(): void {
-    console.log(this.searchQuery)
+  
+  // On lance la recherche dans le serveur
+  onSearch(query: string): void {
+    console.log(query)
+  }
+
+  // Au changement dans le champ de recherche, on lance cette méthode
+  onInputChange(): void {
+    this.searchSubject.next(this.searchQuery)
   }
 
 
