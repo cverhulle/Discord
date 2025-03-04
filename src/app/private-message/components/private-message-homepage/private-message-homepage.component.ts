0d038dfe16295f87279135581211b6d9d4a6adb0
cverhulle <corentin.verhulle@gmail.com>
2025-03-04 11:37:48 +0100

    Création de la barre de recherche dans le template.

--- src/app/private-message/components/private-message-homepage/private-message-homepage.component.ts
+++ src/app/private-message/components/private-message-homepage/private-message-homepage.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-private-message-homepage',
-  imports: [],
+  imports: [
+    FormsModule,
+  ],
   templateUrl: './private-message-homepage.component.html',
   styleUrl: './private-message-homepage.component.scss'
 })
 export class PrivateMessageHomepageComponent {
-
+  // Contient la requete de l'utilisateur
+  searchQuery!: string;
 }
