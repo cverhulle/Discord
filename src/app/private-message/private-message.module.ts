a4c50205ebeaa7db9bb5c7158b72a51b84e10437
cverhulle <corentin.verhulle@gmail.com>
2025-03-04 10:42:30 +0100

    Création d'un nouveau module pour les messages privés. Création du component homepage.

---
+++ src/app/private-message/private-message.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PrivateMessageRoutingModule } from './private-message-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    PrivateMessageRoutingModule
+  ]
+})
+export class PrivateMessageModule { }
