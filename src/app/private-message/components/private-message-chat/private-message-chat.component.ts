ca9a8f9457db92e662153d3a98671b1783838f34
cverhulle <corentin.verhulle@gmail.com>
2025-03-06 10:28:47 +0100

    Création du component private-message-chat

---
+++ src/app/private-message/components/private-message-chat/private-message-chat.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-private-message-chat',
+  imports: [],
+  templateUrl: './private-message-chat.component.html',
+  styleUrl: './private-message-chat.component.scss'
+})
+export class PrivateMessageChatComponent {
+
+}
