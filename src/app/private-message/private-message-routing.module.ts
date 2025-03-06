23a9dd582260be6ba083ebc8ad2b6572c391cfc0
cverhulle <corentin.verhulle@gmail.com>
2025-03-06 10:30:40 +0100

    Création du path vers private-message-chat

--- src/app/private-message/private-message-routing.module.ts
+++ src/app/private-message/private-message-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PrivateMessageHomepageComponent } from './components/private-message-homepage/private-message-homepage.component';
 import { AuthGuard } from '../guard/auth.guard';
+import { PrivateMessageChatComponent } from './components/private-message-chat/private-message-chat.component';
 
 const routes: Routes = [
-  { path: '', component: PrivateMessageHomepageComponent, canActivate : [AuthGuard] }
+  { path: '', component: PrivateMessageHomepageComponent, canActivate : [AuthGuard] },
+  { path: 'chat', component: PrivateMessageChatComponent, canActivate : [AuthGuard] }
 ];
 
 @NgModule({
