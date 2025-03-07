cf33d0e02ac4a4da16d7e1a8102227e84b41a3bf
cverhulle <corentin.verhulle@gmail.com>
2025-03-07 11:17:12 +0100

    Création du service pour envoyer les posts.

---
+++ src/app/shared/post/services/post.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Post } from "../models/post.model";
+import { catchError, map, Observable, of } from "rxjs";
+import { environment } from "../../../../environments/environment.development";
+
+@Injectable() 
+
+export class PostService{
+    constructor(private http: HttpClient){}
+
+    sendPost(post : Post): Observable<boolean> {
+        return this.http.post(`${environment.apiUrl}/private-message/post`, post).pipe(
+            map( () => true),
+            catchError( () => of(false))
+        )
+    }
+}
+
