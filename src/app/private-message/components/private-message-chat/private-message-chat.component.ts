337ce37b2159c14b25b1c1de92d4c143b82665b8
cverhulle <corentin.verhulle@gmail.com>
2025-03-07 11:09:30 +0100

    Création du post à l'appui sur le bouton envoi.

--- src/app/private-message/components/private-message-chat/private-message-chat.component.ts
+++ src/app/private-message/components/private-message-chat/private-message-chat.component.ts
@@ -4,6 +4,8 @@ import { ProfileService } from '../../../profile/service/profile.service';
 import { tap } from 'rxjs';
 import { SharedModule } from '../../../shared/shared.module';
 import { FormsModule } from '@angular/forms';
+import { Post } from '../../../shared/post/models/post.model';
+import { AvatarService } from '../../../shared/avatar/service/avatar.service';
 
 
 @Component({
@@ -27,7 +29,11 @@ export class PrivateMessageChatComponent implements OnInit{
   // Variable pour récupérer le texte dans le message.
   messageContent!: string;
 
-  constructor(private profileService : ProfileService) {}
+  // Variable pour stocker le post à envoyer au backend
+  post!: Post
+
+  constructor(private profileService : ProfileService,
+              private avatarService : AvatarService) {}
 
   ngOnInit(): void {
     this.initCurrentUser()
@@ -58,7 +64,16 @@ export class PrivateMessageChatComponent implements OnInit{
 
 
   onSendMessage(): void{
-    console.log(this.messageContent)
+    this.post = {
+      id : this.currentUser.id,
+      username : this.currentUser.username,
+      image : this.avatarService.getProfileImage(this.currentUser),
+      content : this.messageContent,
+      timestamp : new Date()
+    }
+    console.log(this.post)
+
+
   }
 
 }
