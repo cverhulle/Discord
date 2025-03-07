d8623f4cb4eb2afbed162c909407e69cd2fb0c77
cverhulle <corentin.verhulle@gmail.com>
2025-03-07 11:18:07 +0100

    Ajout du PostService dans le provider.

--- src/app/private-message/private-message.module.ts
+++ src/app/private-message/private-message.module.ts
@@ -5,6 +5,7 @@ import { PrivateMessageRoutingModule } from './private-message-routing.module';
 import { PrivateMessageService } from './service/private-message.service';
 import { ProfileService } from '../profile/service/profile.service';
 import { AvatarService } from '../shared/avatar/service/avatar.service';
+import { PostService } from '../shared/post/services/post.service';
 
 
 @NgModule({
@@ -16,7 +17,8 @@ import { AvatarService } from '../shared/avatar/service/avatar.service';
   providers: [
     PrivateMessageService,
     ProfileService,
-    AvatarService
+    AvatarService,
+    PostService
   ]
 })
 export class PrivateMessageModule { }
