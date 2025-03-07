cd2e02a090fe0b4615ad569e4e183eabad9268d2
cverhulle <corentin.verhulle@gmail.com>
2025-03-07 10:25:08 +0100

    Implémentation du dictionnaire des erreurs dans avatar service

--- src/app/shared/avatar/service/avatar.service.ts
+++ src/app/shared/avatar/service/avatar.service.ts
@@ -1,15 +1,29 @@
 import { Injectable } from "@angular/core";
 import { usernameImage } from "../../../private-message/models/username-image.models";
+import { BehaviorSubject } from "rxjs";
 
 @Injectable( {
     providedIn: 'root'
 })
 
 export class AvatarService{
+    // Dictionnaire pour stocker les erreurs de chargement d'image
+    private imageErrorsSubject = new BehaviorSubject<{ [key: string]: boolean}>({});
+    imageErrors$ = this.imageErrorsSubject.asObservable();
+
     // Variable pour stocker l'URL des images de profil par défaut.
     private defaultUrl: string = 'https://static.vecteezy.com/ti/vecteur-libre/p1/4274186-person-icon-user-interface-icon-silhouette-of-man-simple-symbol-a-glyph-symbol-in-your-web-site-design-logo-app-ui-webinar-video-chat-ect-vectoriel.jpg'
 
-    getProfileImage(user: usernameImage, imageErrors: { [key: string] : boolean }) : string {
-        return imageErrors[user.username] ? this.defaultUrl : user.image
+
+    updateImageError(username: string, hasError: boolean): void {
+        const currentErrors = this.imageErrorsSubject.value;
+        currentErrors[username] = hasError;
+        this.imageErrorsSubject.next(currentErrors)
+    }
+
+
+    getProfileImage(user: usernameImage) : string {
+        const currentErrors = this.imageErrorsSubject.value;
+        return currentErrors[user.username] ? this.defaultUrl : user.image;
     }
 }
