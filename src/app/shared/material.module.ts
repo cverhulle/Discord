3fbffb26008a15f9ab4e376eddbc228ae4949d46
cverhulle <corentin.verhulle@gmail.com>
2025-03-10 16:33:04 +0100

    Importation de matSnackBarModule dans material.module.ts

--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -6,6 +6,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 @NgModule({
   exports: [
@@ -15,7 +16,8 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
-    MatProgressSpinnerModule
+    MatProgressSpinnerModule,
+    MatSnackBarModule
  ]
 })
 
