e45395b47489f94bbc01d52039295853ffdb29d1
cverhulle <corentin.verhulle@gmail.com>
2025-03-11 16:23:55 +0100

    Création d'un service pour gérer la récupération des donnnées des utilisateurs.

---
+++ src/app/shared/post/services/user.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from "@angular/core";
+import { ProfileService } from "../../../profile/service/profile.service";
+import { Observable, tap } from "rxjs";
+import { usernameImage } from "../../../private-message/models/username-image.models";
+
+@Injectable()
+
+export class UserService{
+    constructor(private profileService : ProfileService){}
+
+    getCurrentUser(): Observable<usernameImage>{
+        const currentUser = new usernameImage();
+    
+        return this.profileService.getProfile().pipe(
+            tap((profile: any) => {
+                currentUser['id']= profile.user._id,
+                currentUser['username']=profile.user.loginInfo.username,
+                currentUser['image']=profile.user.image
+                return currentUser
+            })
+        )
+    }
+}
