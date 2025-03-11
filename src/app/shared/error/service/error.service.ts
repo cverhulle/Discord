0d9a41aa9be29efb2edf37355b1dc879d5fb73b7
cverhulle <corentin.verhulle@gmail.com>
2025-03-11 16:48:24 +0100

    Création d'un service error dans sharedModule pour afficher les erreurs.

---
+++ src/app/shared/error/service/error.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from "@angular/core";
+import { MatSnackBar } from "@angular/material/snack-bar";
+
+@Injectable()
+
+export class ErrorService {
+    constructor(private matSnackBar: MatSnackBar) {}
+  
+    displayError(message: string): void {
+      this.matSnackBar.open(message, 'Fermer', {
+        duration: 6000,
+        verticalPosition: 'top',
+        horizontalPosition: 'center'
+      });
+    }
+  }
