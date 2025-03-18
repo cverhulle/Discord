5133b876c876e655f77f4c762c8ce645ee21bf1a
cverhulle <corentin.verhulle@gmail.com>
2025-03-18 15:17:05 +0100

    Ajout de commentaires.

--- src/app/shared/emojis/services/emojis.service.ts
+++ src/app/shared/emojis/services/emojis.service.ts
@@ -3,14 +3,17 @@ import { Injectable } from "@angular/core";
 @Injectable()
 
 export class EmojisService{
+    // Tableau pour exclure des catégories dans le selecteur d'émotes
     categoryExcluded(): [string] {
         return ["flags"]
     }
 
+    // A partir de l'event déclenché au clic sur un émoji, on retourne l'émoji selectionné sous forme de string.
     loadEmojisFromEvent(event: any): string{
         return event.emoji.native
     }
 
+    // A partir de l'event déclenché au clic sur un émoji, on ajoute cet émoji dans le message.
     addEmojisToMessage(event: any, message: string): string{
         const emoji = this.loadEmojisFromEvent(event)
         return message += emoji
