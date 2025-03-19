a380ba2ce3ef9cf5db5ddfde2d124afc83bca3b9
cverhulle <corentin.verhulle@gmail.com>
2025-03-19 16:00:17 +0100

    Ajout de commentaires sur les méthodes et sur l'utilisation de avatarService.

--- src/app/shared/avatar/service/avatar.service.ts
+++ src/app/shared/avatar/service/avatar.service.ts
@@ -1,11 +1,11 @@
 import { Injectable } from "@angular/core";
-import { usernameImage } from "../../../private-message/models/username-image.models";
 import { BehaviorSubject } from "rxjs";
 
 @Injectable( {
     providedIn: 'root'
 })
 
+// Ce service permet de gérer l'affichage des images de profil en petit avatar.
 export class AvatarService{
     // Dictionnaire pour stocker les erreurs de chargement d'image
     private imageErrorsSubject = new BehaviorSubject<{ [key: string]: boolean}>({});
@@ -14,14 +14,14 @@ export class AvatarService{
     // Variable pour stocker l'URL des images de profil par défaut.
     private defaultUrl: string = 'https://static.vecteezy.com/ti/vecteur-libre/p1/4274186-person-icon-user-interface-icon-silhouette-of-man-simple-symbol-a-glyph-symbol-in-your-web-site-design-logo-app-ui-webinar-video-chat-ect-vectoriel.jpg'
 
-
+    // Cette méthode permet de faire émettre au Subject le dictionnaire.
     updateImageError(username: string, hasError: boolean): void {
         const currentErrors = this.imageErrorsSubject.value;
         currentErrors[username] = hasError;
         this.imageErrorsSubject.next(currentErrors)
     }
 
-
+    // Cette méthode permet de retourner l'image par défaut s'il y a une erreur dans l'image de profil de l'utilisateur.
     getProfileImage(userImage: string, username: string) : string {
         const currentErrors = this.imageErrorsSubject.value;
         return currentErrors[username] ? this.defaultUrl : userImage;
