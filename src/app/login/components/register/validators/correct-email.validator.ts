5164dedc0a22803fbe503030615199de129d1b40
cverhulle <corentin.verhulle@gmail.com>
2025-03-19 15:41:12 +0100

    Ajout d'un commentaire pour décrire le but de ce validator.

--- src/app/login/components/register/validators/correct-email.validator.ts
+++ src/app/login/components/register/validators/correct-email.validator.ts
@@ -1,7 +1,9 @@
 import { AbstractControl, ValidationErrors, ValidatorFn } from "@angular/forms";
 
+// Ce Validator permet de vérifier qu'un champ est bien de type "email" (d'après l'expression régulière proposée).
 export function correctEmailValidator(): ValidatorFn {
     return (ctrl: AbstractControl): null | ValidationErrors => {
+        // Cette expression régulière est celle attendue pour l'email.
         let regExp = new RegExp("^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+[.]{1}[a-zA-Z]{2,3}$");
         if (regExp.test(ctrl.value)) {
             return null;
