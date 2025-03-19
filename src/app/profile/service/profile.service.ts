bf4841fada2b2ba359044eb1a1b0228bfd1bf6d5
cverhulle <corentin.verhulle@gmail.com>
2025-03-19 15:57:13 +0100

    Ajout de commentaires pour chaque méthode du service.

--- src/app/profile/service/profile.service.ts
+++ src/app/profile/service/profile.service.ts
@@ -10,11 +10,12 @@ import { RegisterForm } from "../../login/components/register/models/register-fo
 export class ProfileService {
     constructor( private http: HttpClient) {}
 
-    // Cette méthode lance la requête pour récupérer les données sur un utilisteur donc l'id est donné en argument.
+    // Cette méthode lance la requête pour récupérer les données sur un utilisteur dont l'id est donné en argument.
     getProfile(): Observable<any> {
         return this.http.get(`${environment.apiUrl}/profile`)
     }
 
+    // Cette méthode permet de modifier les données de l'utilisateur à partir du formulaire en argument.
     modifyUserInfo(formValue: ModifyProfileForm): Observable<boolean>{
         return this.http.put(`${environment.apiUrl}/profile/modify`, formValue).pipe(
             map( () => true),
@@ -22,6 +23,7 @@ export class ProfileService {
         )
     }
 
+    // Cette méthode permet de modifier le mot de passe de l'utilisateur à partir du formulaire en argument.
     modifyPassword(formValue: RegisterForm): Observable<boolean> {
         return this.http.put(`${environment.apiUrl}/profile/modifyPassword`, formValue).pipe(
             map( () => true),
@@ -29,6 +31,7 @@ export class ProfileService {
         )
     }
 
+    // Cette méthode permet de vérifier si l'email est déjà utilisé à partir du formulaire en argument.
     emailTaken(formValue: ModifyProfileForm) : Observable<boolean>{
         return this.http.post(`${environment.apiUrl}/profile/email`, formValue).pipe(
             map( () => false),
@@ -37,6 +40,7 @@ export class ProfileService {
         ); 
     }
 
+    // Cette méthode permet de vérifier si l'username est déjà utilisé à partir du formulaire en argument.
     usernameTaken(formValue: ModifyProfileForm): Observable<boolean>{
             return this.http.post(`${environment.apiUrl}/profile/username`, formValue).pipe(
                 map( () => false),
