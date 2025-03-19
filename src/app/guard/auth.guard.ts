4d0374c234f4618ad3755e504f89300a5ccf5404
cverhulle <corentin.verhulle@gmail.com>
2025-03-19 15:21:23 +0100

    Suppression des imports inutiles.

--- src/app/guard/auth.guard.ts
+++ src/app/guard/auth.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@angular/core";
-import { ActivatedRouteSnapshot, CanActivate, GuardResult, MaybeAsync, Router, RouterStateSnapshot } from "@angular/router";
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from "@angular/router";
 import { TokenService } from "../interceptors/services/auth.service";
 
 @Injectable({
