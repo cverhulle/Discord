4105e6b48d6621dcce3e72f01304217776f6e51f
cverhulle <corentin.verhulle@gmail.com>
2025-03-28 18:06:16 +0100

    Ajout de imagetoSend à null après la mise à jour d'un message.

--- src/app/private-message/components/private-message-chat/private-message-chat.component.ts
+++ src/app/private-message/components/private-message-chat/private-message-chat.component.ts
@@ -195,7 +195,7 @@ export class PrivateMessageChatComponent implements OnInit{
       this.chat[messageIndex] = updatedPost;
       this.postService.setEditMessage(null)
       this.deleteImageInModifiedPost = false
-      
+      this.imageToSend = null
     })
   }
 
