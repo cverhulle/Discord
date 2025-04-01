cc12ec9299c0a4dcf239523d6047f8b6fd14ff82
cverhulle <corentin.verhulle@gmail.com>
2025-04-01 15:07:53 +0200

    Création d'un nouveau component pour afficher un message pour confirer la suppression du compte.

---
+++ src/app/profile/components/confirm-delete-account/confirm-delete-account.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-confirm-delete-account',
+  imports: [],
+  templateUrl: './confirm-delete-account.component.html',
+  styleUrl: './confirm-delete-account.component.scss'
+})
+export class ConfirmDeleteAccountComponent {
+
+}
