f30388bc1e0dc33b47d63eb4e035567d4cf79627
cverhulle <corentin.verhulle@gmail.com>
2025-04-03 11:23:09 +0200

    Ajout du routing vers le nouveau module.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ export const routes: Routes = [
     {path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule)},
     {path: 'profile', loadChildren: () => import('./profile/profile.module').then(m => m.ProfileModule)},
     {path: 'private-message', loadChildren: () => import('./private-message/private-message.module').then(m => m.PrivateMessageModule)},
+    {path: 'group-message', loadChildren: () => import('./group-message/group-message.module').then(m=> m.GroupMessageModule)},
     {path: '**', redirectTo:'homepage'}
 ];
 
