ed09e0d1c235eac60f79fcef2b4a73dce172aef5
cverhulle <corentin.verhulle@gmail.com>
2025-04-03 11:30:00 +0200

    Création du chemin pour ouvrir le component.

--- src/app/group-message/group-message-routing.module.ts
+++ src/app/group-message/group-message-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomepageGroupMessageComponent } from './components/homepage-gropu-message/homepage-group-message.component';
+import { AuthGuard } from '../guard/auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'homepage', component : HomepageGroupMessageComponent, canActivate : [AuthGuard]}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
