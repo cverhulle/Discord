5b7dbda43d2723e9a12b330a7f34891e9a5b969d
cverhulle <corentin.verhulle@gmail.com>
2025-04-03 11:28:32 +0200

    Création du component pour les messages de groupe.

---
+++ src/app/group-message/components/homepage-gropu-message/homepage-group-message.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-homepage-group-message',
+  imports: [],
+  templateUrl: './homepage-group-message.component.html',
+  styleUrl: './homepage-group-message.component.scss'
+})
+export class HomepageGroupMessageComponent {
+
+}
