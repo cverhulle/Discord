b4feb773ca22d315c078fb57aadabccc57596e01
cverhulle <corentin.verhulle@gmail.com>
2025-04-03 11:19:58 +0200

    Création du module pour les conversations en groupe.

---
+++ src/app/group-message/group-message.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { GroupMessageRoutingModule } from './group-message-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    GroupMessageRoutingModule
+  ]
+})
+export class GroupMessageModule { }
