3494a82cf418d12b043f67ecc919f866dba7dee1
cverhulle <corentin.verhulle@gmail.com>
2025-04-03 16:53:56 +0200

    Import de SharedModule.

--- src/app/group-message/components/homepage-group-message/homepage-group-message.component.ts
+++ src/app/group-message/components/homepage-group-message/homepage-group-message.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { SharedModule } from '../../../shared/shared.module';
 
 @Component({
   selector: 'app-homepage-group-message',
-  imports: [],
+  imports: [
+    SharedModule
+  ],
   templateUrl: './homepage-group-message.component.html',
   styleUrl: './homepage-group-message.component.scss'
 })
