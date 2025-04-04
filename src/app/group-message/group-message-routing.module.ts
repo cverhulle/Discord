731b0ce5ccd878fdce4c581968e288237d422898
cverhulle <corentin.verhulle@gmail.com>
2025-04-04 18:24:44 +0200

    Mise en place du routing vers create-group

--- src/app/group-message/group-message-routing.module.ts
+++ src/app/group-message/group-message-routing.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomepageGroupMessageComponent } from './components/homepage-group-message/homepage-group-message.component';
 import { AuthGuard } from '../guard/auth.guard';
+import { CreateGroupComponent } from './components/create-group/create-group.component';
 
 const routes: Routes = [
+  {path: 'create-group', component: CreateGroupComponent, canActivate: [AuthGuard]},
   {path: '', component : HomepageGroupMessageComponent, canActivate : [AuthGuard]}
 ];
 
