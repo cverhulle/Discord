f93709841d69bf0f585fbadd16ce532f7f7bfc1d
cverhulle <corentin.verhulle@gmail.com>
2025-04-04 18:34:28 +0200

    Import de SharedModule

--- src/app/group-message/components/create-group/create-group.component.ts
+++ src/app/group-message/components/create-group/create-group.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { SharedModule } from '../../../shared/shared.module';
 
 @Component({
   selector: 'app-create-group',
-  imports: [],
+  imports: [
+    SharedModule
+  ],
   templateUrl: './create-group.component.html',
   styleUrl: './create-group.component.scss'
 })
 export class CreateGroupComponent {
 
 }
+ 
