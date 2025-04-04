d7de3ffbade9e19bb3c508dfaaebe5e6b1862647
cverhulle <corentin.verhulle@gmail.com>
2025-04-04 18:28:04 +0200

    Ajout de la redirection au clic sur le bouton "Créer un groupe"

--- src/app/group-message/components/homepage-group-message/homepage-group-message.component.ts
+++ src/app/group-message/components/homepage-group-message/homepage-group-message.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { SharedModule } from '../../../shared/shared.module';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-homepage-group-message',
   imports: [
-    SharedModule
+    SharedModule,
+    RouterLink
   ],
   templateUrl: './homepage-group-message.component.html',
   styleUrl: './homepage-group-message.component.scss'
