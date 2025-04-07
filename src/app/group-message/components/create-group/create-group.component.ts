a1265311ca45a691a5bb59a71777ce2b1fd814b5
cverhulle <corentin.verhulle@gmail.com>
2025-04-07 20:31:17 +0200

    Affichage des catégories avec un ngFor.

--- src/app/group-message/components/create-group/create-group.component.ts
+++ src/app/group-message/components/create-group/create-group.component.ts
@@ -1,15 +1,23 @@
 import { Component } from '@angular/core';
 import { SharedModule } from '../../../shared/shared.module';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-create-group',
   imports: [
-    SharedModule
+    SharedModule,
+    NgFor
   ],
   templateUrl: './create-group.component.html',
   styleUrl: './create-group.component.scss'
 })
 export class CreateGroupComponent {
+  // Variable pour stocker les catégories possibles
+  categories = ['Animé', 'Art', 'Cuisine', 'Éducation',
+    'Films et Séries', 'Jeux vidéos', 'Langues Étrangères',
+    'Manga', 'Musique', 'Photographie', 'Programmation',
+    'Sport', 'Travail', 'Voyage'
+  ];
 
 }
  
