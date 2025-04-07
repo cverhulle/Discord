51a9408684a4aa4122ec81d9cde1fa691addd8c0
cverhulle <corentin.verhulle@gmail.com>
2025-04-07 20:42:07 +0200

    Ajout de commentaires.

--- src/app/group-message/services/chip.service.ts
+++ src/app/group-message/services/chip.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from "@angular/core";
 @Injectable()
 
 export class ChipService{
+    // Cette variable stocke les catégories possibles pour les groupes.
     chipsCategories = [
         'Animé', 'Art', 'Cuisine', 'Éducation','Films et Séries', 
         'Jeux vidéos', 'Langues Étrangères', 'Manga', 'Musique', 
