da3eca8772174f11d63844d0cccbad95206b655e
cverhulle <corentin.verhulle@gmail.com>
2025-04-08 14:59:05 +0200

    Import de chipService.

--- src/app/group-message/services/form-create-group-message.service.ts
+++ src/app/group-message/services/form-create-group-message.service.ts
@@ -1,7 +1,12 @@
 import { Injectable } from "@angular/core";
+import { ChipService } from "./chip.service";
 
 @Injectable()
 
 export class FormCreateGroupMessage {
+    constructor(private chipService : ChipService) {}
 
+    createFormDataToSend(): void{
+        
+    }
 }
