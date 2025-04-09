5a064eb1e8ea87e8379174b7b5fca9a6e4066f3d
cverhulle <corentin.verhulle@gmail.com>
2025-04-09 16:42:43 +0200

    Import de imageSerice.

--- src/app/group-message/services/form-create-group-message.service.ts
+++ src/app/group-message/services/form-create-group-message.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from "@angular/core";
 import { ChipService } from "./chip.service";
 import { FormGroup } from "@angular/forms";
+import { ImageService } from "../../shared/image/services/image.services";
 
 @Injectable()
 
 export class FormCreateGroupMessageService {
-    constructor(private chipService : ChipService) {}
+    constructor(private chipService : ChipService,
+                private imageService : ImageService) {}
 
     // Cette méthode permet de créer le formData à envoyer au backend en prenant le formGroup en argument.
     createFormDataToSend(registerForm: FormGroup): void{
