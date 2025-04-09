75cb93ad15dbd2c673cfb6311ca0abf22c73a51a
cverhulle <corentin.verhulle@gmail.com>
2025-04-09 17:16:34 +0200

    Import de languageService.

--- src/app/group-message/services/form-create-group-message.service.ts
+++ src/app/group-message/services/form-create-group-message.service.ts
@@ -2,17 +2,20 @@ import { Injectable } from "@angular/core";
 import { ChipService } from "./chip.service";
 import { FormGroup } from "@angular/forms";
 import { ImageService } from "../../shared/image/services/image.services";
+import { LanguageService } from "./language.service";
 
 @Injectable()
 
 export class FormCreateGroupMessageService {
     constructor(private chipService : ChipService,
-                private imageService : ImageService) {}
+                private imageService : ImageService,
+                private languageService: LanguageService) {}
 
     // Cette méthode permet de créer le formData à envoyer au backend en prenant le formGroup en argument.
     createFormDataToSend(registerForm: FormGroup): void{
         const selectedCategories = this.chipService.getValueOfSelectedCategories()
         const logoToSend = this.imageService.getValueOfImageToSend()
+        
         const formData = {
           ...registerForm.value,
           categories : selectedCategories,
