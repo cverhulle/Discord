11bbc80d345ba4874eddb683a7561d1677984723
cverhulle <corentin.verhulle@gmail.com>
2025-04-09 16:51:51 +0200

    Création de availableLanguages

--- src/app/group-message/services/language.service.ts
+++ src/app/group-message/services/language.service.ts
@@ -3,5 +3,6 @@ import { Injectable } from "@angular/core";
 @Injectable()
 
 export class LanguageService{
-    
+    // Cette variable permet de stocker les languages disponibles pour la création d'un groupe.
+    availableLanguages = ['Allemand', 'Anglais', 'Espagnol', 'Français']
 }
