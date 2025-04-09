7464fc7734d30e3c736ae970179c6b7bb41f7c4c
cverhulle <corentin.verhulle@gmail.com>
2025-04-09 17:00:18 +0200

    Création de selectedLanguagesSubject

--- src/app/group-message/services/language.service.ts
+++ src/app/group-message/services/language.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from "@angular/core";
+import { BehaviorSubject } from "rxjs";
 
 @Injectable()
 
 export class LanguageService{
     // Cette variable permet de stocker les languages disponibles pour la création d'un groupe.
     availableLanguages = ['Allemand', 'Anglais', 'Espagnol', 'Français'];
+
+    // Subject et Observable pour stocker les languages sélectionnés
+    private selectedLanguagesSubject = new BehaviorSubject<string[]>([])
+    selectedLanguages$ = this.selectedLanguagesSubject.asObservable()
 }
