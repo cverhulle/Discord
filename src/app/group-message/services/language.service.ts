3275da9eff96444894531a938771579068c67f22
cverhulle <corentin.verhulle@gmail.com>
2025-04-09 17:02:11 +0200

    Création de getValueOfSelectedLanguages

--- src/app/group-message/services/language.service.ts
+++ src/app/group-message/services/language.service.ts
@@ -10,4 +10,9 @@ export class LanguageService{
     // Subject et Observable pour stocker les languages sélectionnés
     private selectedLanguagesSubject = new BehaviorSubject<string[]>([])
     selectedLanguages$ = this.selectedLanguagesSubject.asObservable()
+
+    // Méthode pour récupérer la dernière émission de selectedLanguagesSubject
+    getValueOfSelectedLanguages(): string[] {
+        return this.selectedLanguagesSubject.getValue()
+    }
 }
