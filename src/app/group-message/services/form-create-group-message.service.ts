044c66606331ff26ad2eb4809c2426387d454474
cverhulle <corentin.verhulle@gmail.com>
2025-04-09 17:18:14 +0200

    Ajout de commentaires.

--- src/app/group-message/services/form-create-group-message.service.ts
+++ src/app/group-message/services/form-create-group-message.service.ts
@@ -13,16 +13,22 @@ export class FormCreateGroupMessageService {
 
     // Cette méthode permet de créer le formData à envoyer au backend en prenant le formGroup en argument.
     createFormDataToSend(registerForm: FormGroup): void{
-        const selectedCategories = this.chipService.getValueOfSelectedCategories()
-        const selectedLanguages = this.languageService.getValueOfSelectedLanguages()
-        const logoToSend = this.imageService.getValueOfImageToSend()
-        
-        const formData = {
-          ...registerForm.value,
-          categories : selectedCategories,
-          languages : selectedLanguages,
-          logo: logoToSend
-        }
-        console.log(formData)
-    }
+      // On récupère les catégories
+      const selectedCategories = this.chipService.getValueOfSelectedCategories()
+
+      // On récupère les languages
+      const selectedLanguages = this.languageService.getValueOfSelectedLanguages()
+
+      // On récupère le logo
+      const logoToSend = this.imageService.getValueOfImageToSend()
+      
+      // On construit le formData
+      const formData = {
+        ...registerForm.value,
+        categories : selectedCategories,
+        languages : selectedLanguages,
+        logo: logoToSend
+      }
+      console.log(formData)
+  }
 }
