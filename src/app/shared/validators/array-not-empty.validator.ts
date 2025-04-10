c98639c8681028a146f2d41b31920aa4f10d3bcb
cverhulle <corentin.verhulle@gmail.com>
2025-04-10 11:36:11 +0200

    Ajout de la vérification de ctrl contienne bien une valeur.

--- src/app/shared/validators/array-not-empty.validator.ts
+++ src/app/shared/validators/array-not-empty.validator.ts
@@ -2,6 +2,12 @@ import { AbstractControl, ValidationErrors, ValidatorFn } from "@angular/forms";
 
 export function ArrayNotEmpty(): ValidatorFn{
     return (ctrl: AbstractControl): null | ValidationErrors => {
+        if (!ctrl.value) {
+            return {
+                arrayEmpty : 'Invalid control'
+            };
+        }
+        
         const array = ctrl.value
         return Array.isArray(array) && array.length > 0 ? null : {arrayEmpty : true}
     }
