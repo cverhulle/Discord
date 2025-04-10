8473fe5356dfde5c6024d893e4edefdf75fff694
cverhulle <corentin.verhulle@gmail.com>
2025-04-10 15:37:46 +0200

    Ajout de commentaires.

--- src/app/group-message/services/form-create-group-message.service.ts
+++ src/app/group-message/services/form-create-group-message.service.ts
@@ -10,15 +10,19 @@ export class FormCreateGroupMessageService {
 
     // Cette méthode permet de créer le formData à envoyer au backend en prenant le formGroup en argument.
     createFormDataToSend(registerForm: FormGroup): FormData{
-     
+      
+      // On récupère les données du formulaire
       const formValues = registerForm.value as CreateGroupForm
 
+      // On initialise le formData
       const formData = new FormData();
-    
+      
+      // On ajoute les champs de texte
       formData.append('groupName', formValues.groupName);
       formData.append('groupDescription', formValues.groupDescription);
       formData.append('groupType', formValues.groupType);
-    
+      
+      // Pour les languages et les catégories, on s'adapte au fait que ce sont des arrays
       formValues.groupLanguages.forEach((lang: string) => {
         formData.append('groupLanguages', lang);
       });
@@ -26,7 +30,8 @@ export class FormCreateGroupMessageService {
       formValues.groupCategories.forEach((cat: string) => {
         formData.append('groupCategories', cat);
       });
-    
+      
+      // S'il y a un logo, on l'ajoute également
       if (formValues.groupLogo) {
         formData.append('groupLogo', formValues.groupLogo);
       }
