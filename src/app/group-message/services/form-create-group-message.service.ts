a40f9ce7e089a5e90311dce307476022cd5d6053
cverhulle <corentin.verhulle@gmail.com>
2025-04-10 15:33:10 +0200

    Implémentation de createformdatatosend.

--- src/app/group-message/services/form-create-group-message.service.ts
+++ src/app/group-message/services/form-create-group-message.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from "@angular/core";
+import { GroupForm } from "../models/group-form.model";
 import { FormGroup } from "@angular/forms";
 
 
@@ -8,12 +9,28 @@ export class FormCreateGroupMessageService {
     constructor() {}
 
     // Cette méthode permet de créer le formData à envoyer au backend en prenant le formGroup en argument.
-    createFormDataToSend(registerForm: FormGroup): void{
+    createFormDataToSend(registerForm: FormGroup): FormData{
      
-      // On construit le formData
-      const formData = {
-        ...registerForm.value,
+      const formValues = registerForm.value as GroupForm
+
+      const formData = new FormData();
+    
+      formData.append('groupName', formValues.groupName);
+      formData.append('groupDescription', formValues.groupDescription);
+      formData.append('groupType', formValues.groupType);
+    
+      formValues.groupLanguages.forEach((lang: string) => {
+        formData.append('groupLanguages', lang);
+      });
+    
+      formValues.groupCategories.forEach((cat: string) => {
+        formData.append('groupCategories', cat);
+      });
+    
+      if (formValues.groupLogo) {
+        formData.append('groupLogo', formValues.groupLogo);
       }
-      console.log(formData)
-  }
+    
+      return formData;
+    }
 }
