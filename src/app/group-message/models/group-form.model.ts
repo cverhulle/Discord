868579deb27ec45e65ca3472ded02c7582a60cec
cverhulle <corentin.verhulle@gmail.com>
2025-04-10 15:29:50 +0200

    Création du modèle GroupForm

---
+++ src/app/group-message/models/group-form.model.ts
@@ -0,0 +1,8 @@
+export class GroupForm {
+    groupName!: string;
+    groupDescription!: string;
+    groupType!: string;
+    groupLanguages!: string[];
+    groupCategories!: string[];
+    groupLogo!: File | null;
+  }
