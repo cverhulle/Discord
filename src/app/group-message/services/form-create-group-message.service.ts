b2a7c1b3d8c1388864a2f3b360d2653b0582de20
cverhulle <corentin.verhulle@gmail.com>
2025-04-10 15:07:53 +0200

    On supprime l'appel au service ImageService.

--- src/app/group-message/services/form-create-group-message.service.ts
+++ src/app/group-message/services/form-create-group-message.service.ts
@@ -1,22 +1,18 @@
 import { Injectable } from "@angular/core";
 import { FormGroup } from "@angular/forms";
-import { ImageService } from "../../shared/image/services/image.services";
 
 
 @Injectable()
 
 export class FormCreateGroupMessageService {
-    constructor(private imageService : ImageService) {}
+    constructor() {}
 
     // Cette méthode permet de créer le formData à envoyer au backend en prenant le formGroup en argument.
     createFormDataToSend(registerForm: FormGroup): void{
-      // On récupère le logo
-      const logoToSend = this.imageService.getValueOfImageToSend()
-      
+     
       // On construit le formData
       const formData = {
         ...registerForm.value,
-        logo: logoToSend
       }
       console.log(formData)
   }
