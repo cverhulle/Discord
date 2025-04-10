bac5a42f6f9cd7c69d2d8d00989a33df6e228cf0
cverhulle <corentin.verhulle@gmail.com>
2025-04-10 11:37:26 +0200

    Ajout de commentaires.

--- src/app/shared/validators/array-not-empty.validator.ts
+++ src/app/shared/validators/array-not-empty.validator.ts
@@ -2,13 +2,18 @@ import { AbstractControl, ValidationErrors, ValidatorFn } from "@angular/forms";
 
 export function ArrayNotEmpty(): ValidatorFn{
     return (ctrl: AbstractControl): null | ValidationErrors => {
+
+        // On vérifie que le ctrl possède bien une value
         if (!ctrl.value) {
             return {
                 arrayEmpty : 'Invalid control'
             };
         }
-        
+
+        // On récupère la value du ctrl
         const array = ctrl.value
+
+        // On vérifie que la valeur obtenue est bien une liste et qu'elle possède au moins un élément
         return Array.isArray(array) && array.length > 0 ? null : {arrayEmpty : true}
     }
 }
