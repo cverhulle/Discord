20d4bb96244bd1a61d19a94409db6e7e400bb865
cverhulle <corentin.verhulle@gmail.com>
2025-04-11 15:21:38 +0200

    On ajoute un if pour vérifier que le controller à une value.

--- src/app/shared/validators/strong-password.validator.ts
+++ src/app/shared/validators/strong-password.validator.ts
@@ -2,9 +2,14 @@ import { AbstractControl, ValidationErrors, ValidatorFn } from "@angular/forms";
 
 export function strongPasswordValidator(): ValidatorFn {
     return (ctrl: AbstractControl): ValidationErrors | null => {
-      const value = ctrl.value || '';
-      const strongPasswordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/;
-  
-      return strongPasswordPattern.test(value)? null : { weakPassword: true };
+
+        if(!ctrl.value){
+            return {weakPassword : 'Invalid controller'}
+        }
+        
+        const value = ctrl.value || '';
+        const strongPasswordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/;
+    
+        return strongPasswordPattern.test(value)? null : { weakPassword: true };
     };
   }
