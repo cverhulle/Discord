02d5b23ef39f9cca22c0a47db7c2209997809129
cverhulle <corentin.verhulle@gmail.com>
2025-04-11 15:23:02 +0200

    Ajout de commentaires.

--- src/app/shared/validators/strong-password.validator.ts
+++ src/app/shared/validators/strong-password.validator.ts
@@ -3,11 +3,14 @@ import { AbstractControl, ValidationErrors, ValidatorFn } from "@angular/forms";
 export function strongPasswordValidator(): ValidatorFn {
     return (ctrl: AbstractControl): ValidationErrors | null => {
 
+        // On vérifie que le ctrl possède bien une value
         if(!ctrl.value){
             return {weakPassword : 'Invalid controller'}
         }
-        
+
         const value = ctrl.value || '';
+
+        // Expression régulière : au moins une minuscule, une majuscule, un chiffre et un caractère spécial.
         const strongPasswordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/;
     
         return strongPasswordPattern.test(value)? null : { weakPassword: true };
