d4dc681251049e41cbb4c74d15f34d6afdbfb37e
cverhulle <corentin.verhulle@gmail.com>
2025-04-11 15:07:36 +0200

    Ajout de l'attribut groupPassword.

--- src/app/group-message/models/group-form.model.ts
+++ src/app/group-message/models/group-form.model.ts
@@ -2,6 +2,7 @@ export class CreateGroupForm {
     groupName!: string;
     groupDescription!: string;
     groupType!: string;
+    groupPassword!: string;
     groupLanguages!: string[];
     groupCategories!: string[];
     groupLogo!: File | null;
