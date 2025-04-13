31c3b09800c370f764c6225387cccbe344a4a100
cverhulle <corentin.verhulle@gmail.com>
2025-04-13 11:47:03 +0200

    Création de getValueOfHidePasswordSubject

--- src/app/shared/password/service/password.service.ts
+++ src/app/shared/password/service/password.service.ts
@@ -8,4 +8,11 @@ export class PasswordService{
     // Subject et Observable pour gérer la visibilité d'un mot de passe.
     private hidePasswordSubject = new BehaviorSubject<boolean>(true);
     hidePassword$ = this.hidePasswordSubject.asObservable();
+
+    // Méthode pour récupérer la dernière valeur émise par le subject hidePassword.
+    getValueOfHidePasswordSubject(): boolean {
+        return this.hidePasswordSubject.getValue()
+    }
+
+    
 }
