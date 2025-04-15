714be060245b4ab3be235c4dbfa6e35c4fdae97e
cverhulle <corentin.verhulle@gmail.com>
2025-04-15 11:43:00 +0200

    Création du component my-group.

---
+++ src/app/group-message/components/my-group/my-group.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-group',
+  imports: [],
+  templateUrl: './my-group.component.html',
+  styleUrl: './my-group.component.scss'
+})
+export class MyGroupComponent {
+
+}
