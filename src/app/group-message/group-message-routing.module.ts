94ba48ea8086c31d74c11cc6ac20d5aa6c9cada8
cverhulle <corentin.verhulle@gmail.com>
2025-04-15 11:43:49 +0200

    Routing vers mygroup.

--- src/app/group-message/group-message-routing.module.ts
+++ src/app/group-message/group-message-routing.module.ts
@@ -3,9 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomepageGroupMessageComponent } from './components/homepage-group-message/homepage-group-message.component';
 import { AuthGuard } from '../guard/auth.guard';
 import { CreateGroupComponent } from './components/create-group/create-group.component';
+import { MyGroupComponent } from './components/my-group/my-group.component';
 
 const routes: Routes = [
   {path: 'create-group', component: CreateGroupComponent, canActivate: [AuthGuard]},
+  {path: 'my-group', component: MyGroupComponent, canActivate: [AuthGuard]},
   {path: '', component : HomepageGroupMessageComponent, canActivate : [AuthGuard]}
 ];
 
