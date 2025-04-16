ec6b421f6c523335c3dc7bb82048dcc5079e0316
cverhulle <corentin.verhulle@gmail.com>
2025-04-16 17:15:51 +0200

    Import de sharedModule Bouton pour créer un compte

--- src/app/homepage/components/homepage/homepage.component.ts
+++ src/app/homepage/components/homepage/homepage.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { CoreModule } from '../../../core/core.module';
+import { SharedModule } from '../../../shared/shared.module';
 
 @Component({
   selector: 'app-homepage',
   imports: [
-    
+    SharedModule
   ],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.scss'
