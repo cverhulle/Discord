54fac931f31f09f948aacca2741c77f70bf0c5fc
cverhulle <corentin.verhulle@gmail.com>
2025-04-22 21:33:22 +0200

    Ajout d'un commentaire.

--- src/app/shared/validators/logo-required.validator.ts
+++ src/app/shared/validators/logo-required.validator.ts
@@ -1,5 +1,6 @@
 import { AbstractControl, ValidationErrors, ValidatorFn } from "@angular/forms";
 
+// Ce Validator permet de vérifier que le logo est bien présent lors de la création d'un groupe.
 export function LogoRequiredValidator() : ValidatorFn{
     return (ctrl: AbstractControl) : ValidationErrors | null => {
 
