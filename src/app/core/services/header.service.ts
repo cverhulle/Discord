d76cfc7028f41399281ca304dd32ef6af53616e3
cverhulle <corentin.verhulle@gmail.com>
2025-04-29 10:54:16 +0200

    Ajout de commentaires

--- src/app/core/services/header.service.ts
+++ src/app/core/services/header.service.ts
@@ -3,7 +3,10 @@ import { BehaviorSubject } from "rxjs";
 
 @Injectable()
 
+// Service lié au header (barre de navigation)
 export class HeaderService{
+
+    // Ce subject permet de controler l'ouverture du menu à l'appui sur le menu burger
     openMenuSubject = new BehaviorSubject<boolean>(false)
     openMenu$ = this.openMenuSubject.asObservable()
 }
