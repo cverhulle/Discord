c95c2070459f4c0ad17101803a20fad5c0c5cfb7
cverhulle <corentin.verhulle@gmail.com>
2025-04-30 17:39:05 +0200

    Ajout d'un commentaire.

--- src/app/shared/searchQuery/services/searchQuery.service.ts
+++ src/app/shared/searchQuery/services/searchQuery.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@angular/core";
 
 @Injectable()
 
+// Ce service permet de gérer la barre de recherches (des utilisateurs et des groupes)
 export class SearchQueryService{
-    
+
 }
