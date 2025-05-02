6ba6ea80ce93a5450f6ce6209064d2ac0ece488b
cverhulle <corentin.verhulle@gmail.com>
2025-05-02 17:46:53 +0200

    Suppression du dossier searchQuery dans shared

--- src/app/shared/searchQuery/services/searchQuery.service.ts
+++
@@ -1,7 +0,0 @@
-import { Injectable } from "@angular/core";
-
-@Injectable()
-
-// Ce service permet de gérer la barre de recherches (des utilisateurs et des groupes)
-export class SearchQueryService{
-}
