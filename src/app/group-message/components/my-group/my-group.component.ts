ac0e6ac9089eb3d2a63c16476606f771b1a7d97a
cverhulle <corentin.verhulle@gmail.com>
2025-05-08 21:59:17 +0200

    Import de onInit et du service searchAGroupService

--- src/app/group-message/components/my-group/my-group.component.ts
+++ src/app/group-message/components/my-group/my-group.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { GroupFormInfo } from '../../models/group-info.model';
+import { SearchAGroupService } from '../../services/search-a-group.service';
 
 @Component({
   selector: 'app-my-group',
@@ -7,7 +8,13 @@ import { GroupFormInfo } from '../../models/group-info.model';
   templateUrl: './my-group.component.html',
   styleUrl: './my-group.component.scss'
 })
-export class MyGroupComponent {
+export class MyGroupComponent implements OnInit {
   // Variable pour stocker les différents groupes de l'utilisateur
   groups!: GroupFormInfo[]
+
+  constructor(private searchAGroupService : SearchAGroupService) {}
+
+  ngOnInit(): void {
+    
+  }
 }
