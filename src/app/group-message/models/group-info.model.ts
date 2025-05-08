789236ffce7e5d7247fc621144d7fe5bcf4b5dd0
cverhulle <corentin.verhulle@gmail.com>
2025-05-08 21:51:46 +0200

    Modèle pour stocker les données des groupes dans le frontend

--- src/app/group-message/models/group-info.model.ts
+++ src/app/group-message/models/group-info.model.ts
@@ -1,1 +1,9 @@
 // Ce modèle est utilisé pour stocker toutes les informations à stocker pour l'affichage dans le template
+export class GroupFormInfo {
+    groupName!: string;
+    groupDescription!: string;
+    groupType!: string;
+    groupLanguages!: string[];
+    groupCategories!: string[];
+    groupLogo!: File | null;
+}
