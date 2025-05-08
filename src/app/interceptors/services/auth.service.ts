df014d293d47a18e44ce1f979d0085cb50ef5d79
cverhulle <corentin.verhulle@gmail.com>
2025-05-08 20:38:34 +0200

    Début d'implémentation de isTokenExpired

--- src/app/interceptors/services/auth.service.ts
+++ src/app/interceptors/services/auth.service.ts
@@ -31,5 +31,16 @@ export class TokenService {
         this.router.navigateByUrl('/homepage')
     }
 
+    private isTokenExpired(token: string): boolean {
+        try {
+            
+            const payload = JSON.parse(atob(token.split('.')[1]));
+            const exp = payload.exp;
+            const now = Math.floor(Date.now() / 1000);
+            return exp < now;
+        } catch (e) {
+        return true; 
+        }
+    }
     
 }
