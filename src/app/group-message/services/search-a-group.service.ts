16c0aac679ff9433ffdb983bfde9775f382fac9d
cverhulle <corentin.verhulle@gmail.com>
2025-05-08 22:23:26 +0200

    Retour sur l'implémentation de getUsersGroups

--- src/app/group-message/services/search-a-group.service.ts
+++ src/app/group-message/services/search-a-group.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { BehaviorSubject, catchError, map, Observable, of } from "rxjs";
+import { BehaviorSubject, catchError, map, Observable, of, tap } from "rxjs";
 import { GroupFormInfo } from "../models/group-info.model";
 import { environment } from "../../../environments/environment.development";
 
@@ -26,9 +26,12 @@ export class SearchAGroupService{
 
     // Cette méthode permet de récupérer tous les groupes dans lesquels l'utilisateur est un membre.
     getUsersGroups(): Observable<boolean> {
-        return this.http.get<GroupFormInfo>(`${environment.apiUrl}/group-message/my-group`).pipe(
+        return this.http.get<GroupFormInfo[]>(`${environment.apiUrl}/group-message/my-group`).pipe(
+            tap( groupList => {
+                this.setValueOfCurrentUserGroupSubject(groupList)
+            }),
             map( () => true),
-            catchError( () => of(false) )
+            catchError( () => of(false))
         )
     }
 }
