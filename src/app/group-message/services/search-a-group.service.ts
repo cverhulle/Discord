6c424e3f50ae0f6ac13f7851d314a79ee7ac0030
cverhulle <corentin.verhulle@gmail.com>
2025-05-08 22:07:13 +0200

    Injection de httpClient + commentaire

--- src/app/group-message/services/search-a-group.service.ts
+++ src/app/group-message/services/search-a-group.service.ts
@@ -1,7 +1,11 @@
+import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 
 @Injectable() 
 
+// Ce service permet de récupérer les groupes de l'utilisateur
 export class SearchAGroupService{
-    
+    constructor(http: HttpClient) {}
+
+
 }
