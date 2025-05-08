eaf3186c649bd93a9b94362cded7672c2d7479a9
cverhulle <corentin.verhulle@gmail.com>
2025-05-08 22:12:06 +0200

    Implémentation de getUsersGroups()

--- src/app/group-message/services/search-a-group.service.ts
+++ src/app/group-message/services/search-a-group.service.ts
@@ -1,11 +1,19 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { catchError, map, Observable, of } from "rxjs";
+import { GroupFormInfo } from "../models/group-info.model";
+import { environment } from "../../../environments/environment.development";
 
 @Injectable() 
 
 // Ce service permet de récupérer les groupes de l'utilisateur
 export class SearchAGroupService{
-    constructor(http: HttpClient) {}
+    constructor(private http: HttpClient) {}
 
-
+    getUsersGroups(): Observable<boolean> {
+        return this.http.get<GroupFormInfo>(`${environment.apiUrl}/group-message/my-group`).pipe(
+            map( () => true),
+            catchError( () => of(false) )
+        )
+    }
 }
