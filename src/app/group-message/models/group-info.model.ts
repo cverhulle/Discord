ad15f9df4c1ed21f496c12493d9435e31a4715ad
cverhulle <corentin.verhulle@gmail.com>
2025-05-08 21:52:14 +0200

    On ajoute le champ _id pour stocker l'id de mongoDB.

--- src/app/group-message/models/group-info.model.ts
+++ src/app/group-message/models/group-info.model.ts
@@ -1,5 +1,6 @@
 // Ce modèle est utilisé pour stocker toutes les informations à stocker pour l'affichage dans le template
 export class GroupFormInfo {
+    _id!: string;
     groupName!: string;
     groupDescription!: string;
     groupType!: string;
