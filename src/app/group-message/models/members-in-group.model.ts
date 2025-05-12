42c944aae5f3864a5ea0d8bfa57f15fdc98f6e66
cverhulle <corentin.verhulle@gmail.com>
2025-05-12 15:20:13 +0200

    Ajout d'un commentaire pour décrire l'utilité de ce modèle

--- src/app/group-message/models/members-in-group.model.ts
+++ src/app/group-message/models/members-in-group.model.ts
@@ -1,3 +1,4 @@
+// Ce modèle permet de stocker la forme des données contenues dans le creator et le member du modèle GroupFormInfo
 export class MembersInGroup{
     id!: string;
     image!: string;
