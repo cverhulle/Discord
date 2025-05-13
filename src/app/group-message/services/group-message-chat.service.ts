1530e6e2786a9f9dfd325a82235d4bda0a167950
cverhulle <corentin.verhulle@gmail.com>
2025-05-13 11:22:39 +0200

    On implémente un subject pour gérer le loading

--- src/app/group-message/services/group-message-chat.service.ts
+++ src/app/group-message/services/group-message-chat.service.ts
@@ -1,7 +1,15 @@
 import { Injectable } from "@angular/core";
+import { BehaviorSubject } from "rxjs";
 
 @Injectable()
 
 export class GroupMessageService{
-    
+    // Observable et Subject pour gérer le chargement
+    private loadingSubject = new BehaviorSubject<boolean>(true)
+    loading$ = this.loadingSubject.asObservable()
+
+    // Méthode pour gérer l'état du chargement
+    setValueOfLoading(state : boolean): void {
+        this.loadingSubject.next(state)
+    }
 }
