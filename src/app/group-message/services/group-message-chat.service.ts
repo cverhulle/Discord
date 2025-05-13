b6cb831361a0ee6bbed19a58557c1359f3197a73
cverhulle <corentin.verhulle@gmail.com>
2025-05-13 11:42:00 +0200

    Récupération des méthodes liées au chat

--- src/app/group-message/services/group-message-chat.service.ts
+++ src/app/group-message/services/group-message-chat.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from "@angular/core";
 import { BehaviorSubject } from "rxjs";
+import { GroupPost } from "../models/group-post.model";
 
 @Injectable()
 
@@ -11,5 +12,19 @@ export class GroupMessageService{
     // Méthode pour gérer l'état du chargement
     setValueOfLoading(state : boolean): void {
         this.loadingSubject.next(state)
+    }
+
+    // Observable et Subject pour gérer le chat.
+    private chatSubject = new  BehaviorSubject<GroupPost[]>([])
+    chat$ = this.chatSubject.asObservable()
+
+    // Méthode pour gérer l'état du chat.
+    setValueOfChat(post : GroupPost[]): void {
+        this.chatSubject.next(post)
+    }
+
+    // Méthode pour récupérer la valeur du chat
+    getValueOfChat(): GroupPost[] {
+        return this.chatSubject.getValue()
     }
 }
