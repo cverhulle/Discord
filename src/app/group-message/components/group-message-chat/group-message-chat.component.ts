1b4ec8d47d4f01487aaef1011305f7641fc4c4ec
cverhulle <corentin.verhulle@gmail.com>
2025-05-13 10:38:18 +0200

    Commencons par dupliquer la logique de private-message-chat dans group-message-chat. On récupère les variables en modifiant "otherUser" par groupName

--- src/app/group-message/components/group-message-chat/group-message-chat.component.ts
+++ src/app/group-message/components/group-message-chat/group-message-chat.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { usernameImage } from '../../../private-message/models/username-image.models';
+import { Observable } from 'rxjs';
+import { Post } from '../../../shared/post/models/post.model';
 
 @Component({
   selector: 'app-group-message-chat',
@@ -6,6 +9,54 @@ import { Component } from '@angular/core';
   templateUrl: './group-message-chat.component.html',
   styleUrl: './group-message-chat.component.scss'
 })
-export class GroupMessageChatComponent {
+export class GroupMessageChatComponent implements OnInit{
+  // Directive pour scroller el chat en bas après l'envoi d'un nouveau message.
+  @ViewChild('chatContainer') chatContainer!: ElementRef
+  
+  // Variable pour récupérer les données de l'utilisateur actuel.
+  currentUser!: usernameImage;
+
+
+
+
+  // Variable pour récupérer le nom du groupe
+  groupName : string = '';
+
+
+
+
+
+  // Variable pour récupérer le texte dans le message.
+  messageContent: string = ''
+
+  // Observable qui stocke la discussion entre les utilisateurs
+  chat$!: Observable<Post[]>
+
+  // Observable pour gérer le loading
+  loading$!: Observable<boolean>
+
+  // Variable pour vérifier si l'historique de discussion entre deux utilisateurs est vide ou non
+  isChatEmpty$!: Observable<boolean>
+
+  // Observable pour réagir lorsque l'utilisateur modifie un message.
+  editMessage$!: Observable<Post | null>;
+
+  // Observable pour gérer l'affichage du selecteur d'émotes.
+  showEmojisList$!: Observable<boolean>
+
+  // Variable pour stocker les catégories d'émojis à ne pas charger.
+  categoriesEmojisExcluded!: [string] 
+
+  // Observable pour gérer l'image à envoyer dans le Post.
+  imageToSend$! : Observable<File | null>;
+
+  // Observable pour gérer si une photo est supprimé dans le Post à modifier.
+  deleteImageInModifiedPost$!: Observable<boolean>
+
+  // Observable pour stocker la conversion en URL de l'image à envoyer
+  imageToSendUrl$!: Observable<string | null>
+
+  // Observable pour gérer l'opacité du bandeau de prévisualisation
+  opacityPreview$!: Observable<number>
 
 }
