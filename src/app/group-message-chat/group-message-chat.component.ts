f46bcd4b2dfb3a3501a26a404a89fd2de04a3f7b
cverhulle <corentin.verhulle@gmail.com>
2025-05-13 10:11:54 +0200

    Création du component pour la discussion de groupe

---
+++ src/app/group-message-chat/group-message-chat.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-group-message-chat',
+  imports: [],
+  templateUrl: './group-message-chat.component.html',
+  styleUrl: './group-message-chat.component.scss'
+})
+export class GroupMessageChatComponent {
+
+}
