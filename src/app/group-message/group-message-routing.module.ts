501d38b7be068cc5fc8d3db407a000406282d151
cverhulle <corentin.verhulle@gmail.com>
2025-05-19 10:46:55 +0200

    Ajout du path vers rejoindre un groupe

--- src/app/group-message/group-message-routing.module.ts
+++ src/app/group-message/group-message-routing.module.ts
@@ -5,12 +5,14 @@ import { AuthGuard } from '../guard/auth.guard';
 import { CreateGroupComponent } from './components/create-group/create-group.component';
 import { MyGroupComponent } from './components/my-group/my-group.component';
 import { GroupMessageChatComponent } from './components/group-message-chat/group-message-chat.component';
+import { JoinAGroupComponent } from './components/join-a-group/join-a-group.component';
 
 const routes: Routes = [
   {path: 'create-group', component: CreateGroupComponent, canActivate: [AuthGuard]},
   {path: 'my-group', component: MyGroupComponent, canActivate: [AuthGuard]},
   {path: 'chat', component: GroupMessageChatComponent, canActivate: [AuthGuard]},
-  {path: '', component : HomepageGroupMessageComponent, canActivate : [AuthGuard]},
+  {path : 'join-a-group', component : JoinAGroupComponent, canActivate:[AuthGuard]},
+  {path: '', component : HomepageGroupMessageComponent, canActivate : [AuthGuard]}
 ];
 
 @NgModule({
