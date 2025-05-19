2f4c686baf1d65ef5275dd5219d2a5c3c98f44fa
cverhulle <corentin.verhulle@gmail.com>
2025-05-19 10:45:58 +0200

    Création du component pour rejoindre un  groupe

---
+++ src/app/group-message/components/join-a-group/join-a-group.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-join-a-group',
+  imports: [],
+  templateUrl: './join-a-group.component.html',
+  styleUrl: './join-a-group.component.scss'
+})
+export class JoinAGroupComponent {
+
+}
