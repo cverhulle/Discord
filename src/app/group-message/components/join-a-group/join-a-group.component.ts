8569d86d0dacccd5827fec484a0c786f9099026a
cverhulle <corentin.verhulle@gmail.com>
2025-05-19 15:15:09 +0200

    Ajout de la méthode resetSubjects

--- src/app/group-message/components/join-a-group/join-a-group.component.ts
+++ src/app/group-message/components/join-a-group/join-a-group.component.ts
@@ -27,12 +27,19 @@ export class JoinAGroupComponent implements OnInit{
 
   ngOnInit(): void {
       this.initChipsCategoriesAndAvailableLanguages()
+      this.resetSubjects()
   }
 
   // Méthode pour initialiser les catégories disponibles et le subject lié.
   private initChipsCategoriesAndAvailableLanguages(): void{
     this.chipsCategories = this.chipService.chipsCategories
     this.availableLanguages = this.languageService.availableLanguages
+  }
+
+  // Méthode pour réinitialiser les subjects dans le service
+  private resetSubjects(): void{
+    this.chipService.resetSelectedCategoriesSubject()
+    this.languageService.resetSelectedLanguagesSubject()
   }
 
 }
