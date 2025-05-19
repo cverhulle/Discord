dd87fcae61d98b76d912c6aa18cd9ce8c9177c40
cverhulle <corentin.verhulle@gmail.com>
2025-05-19 11:01:28 +0200

    Mise en place de la barre de recherche

--- src/app/group-message/components/join-a-group/join-a-group.component.ts
+++ src/app/group-message/components/join-a-group/join-a-group.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { SharedModule } from '../../../shared/shared.module';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-join-a-group',
-  imports: [],
+  imports: [
+    SharedModule,
+    FormsModule
+  ],
   templateUrl: './join-a-group.component.html',
   styleUrl: './join-a-group.component.scss'
 })
