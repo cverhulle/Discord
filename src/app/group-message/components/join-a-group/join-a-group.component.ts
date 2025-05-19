65c0338a8cc921d97b4276b9cbab24978264a2e9
cverhulle <corentin.verhulle@gmail.com>
2025-05-19 11:07:54 +0200

    Ajout d'un mat-label

--- src/app/group-message/components/join-a-group/join-a-group.component.ts
+++ src/app/group-message/components/join-a-group/join-a-group.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 import { SharedModule } from '../../../shared/shared.module';
-import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-join-a-group',
   imports: [
     SharedModule,
-    FormsModule
   ],
   templateUrl: './join-a-group.component.html',
   styleUrl: './join-a-group.component.scss'
