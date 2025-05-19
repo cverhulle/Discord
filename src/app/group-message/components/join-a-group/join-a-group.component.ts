eedc8ee7627adc5d00cf31e7027cbd295dffedf2
cverhulle <corentin.verhulle@gmail.com>
2025-05-19 14:29:08 +0200

    Import de ngFor et ngIf Squelette des langues de groupe

--- src/app/group-message/components/join-a-group/join-a-group.component.ts
+++ src/app/group-message/components/join-a-group/join-a-group.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { SharedModule } from '../../../shared/shared.module';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-join-a-group',
   imports: [
     SharedModule,
+    NgIf,
+    NgFor
   ],
   templateUrl: './join-a-group.component.html',
   styleUrl: './join-a-group.component.scss'
