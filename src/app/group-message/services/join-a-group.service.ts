aea3ad99538d8a7370d6c0b85c605de10010c98a
cverhulle <corentin.verhulle@gmail.com>
2025-05-20 15:19:36 +0200

    Ajout d'un commentaire et du constructor

--- src/app/group-message/services/join-a-group.service.ts
+++ src/app/group-message/services/join-a-group.service.ts
@@ -1,7 +1,10 @@
+import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 
 @Injectable()
 
+// Ce service permet de gérer l'envoi du formulaire pour rejoindre un groupe
 export class JoinAGroupService{
-    
+
+    constructor(private hhtp: HttpClient) {}
 }
