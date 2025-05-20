d843e3975eca7efdaa2b3798c1ff21d60875495f
cverhulle <corentin.verhulle@gmail.com>
2025-05-20 15:49:52 +0200

    Correction de l'erreur de frappe sur http dans le constructor. Début de l'implémentation de joinGroups

--- src/app/group-message/services/join-a-group.service.ts
+++ src/app/group-message/services/join-a-group.service.ts
@@ -1,13 +1,16 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { JoinAGroup } from "../models/join-a-group.model";
+import { Observable } from "rxjs";
+import { GroupFormInfo } from "../models/group-info.model";
+import { environment } from "../../../environments/environment.development";
 
 @Injectable()
 
 // Ce service permet de gérer l'envoi du formulaire pour rejoindre un groupe
 export class JoinAGroupService{
 
-    constructor(private hhtp: HttpClient) {}
+    constructor(private http: HttpClient) {}
 
     // Cette méthode permet de supprimer les critères que l'utilisateur n'a pas filtrés
     handleSearchCriterias(criteria : JoinAGroup) : JoinAGroup {
@@ -16,5 +19,11 @@ export class JoinAGroupService{
         if(criteria.groupLanguages?.length === 0) delete criteria.groupLanguages
         if( criteria.groupCategories?.length === 0) delete criteria.groupCategories
         return criteria
+    }
+
+    // Cette méthode permet d'envoyer au backend les critères de recherche
+    // Elle retourne les groupes correspondant à la recherche
+    joinGroups(criteria : JoinAGroup) : void {
+        this.http.post<GroupFormInfo>(`${environment.apiUrl}/group-message/join-a-group`, criteria)
     }
 }
