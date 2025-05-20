241122bf237b07328d064ef6f5ebec061a9b1bba
cverhulle <corentin.verhulle@gmail.com>
2025-05-20 15:29:11 +0200

    On retourne la liste des critères mise à jour dans handleSearchCriterias

--- src/app/group-message/services/join-a-group.service.ts
+++ src/app/group-message/services/join-a-group.service.ts
@@ -10,10 +10,11 @@ export class JoinAGroupService{
     constructor(private hhtp: HttpClient) {}
 
     // Cette méthode permet de supprimer les critères que l'utilisateur n'a pas filtrés
-    handleSearchCriterias(criteria : SearchAGroup) {
+    handleSearchCriterias(criteria : SearchAGroup) : SearchAGroup {
         if (!criteria.groupName) delete criteria.groupName
         if(criteria.groupType === 'Tout') delete criteria.groupType
         if(criteria.groupLanguages?.length === 0) delete criteria.groupLanguages
         if( criteria.groupCategories?.length === 0) delete criteria.groupCategories
+        return criteria
     }
 }
