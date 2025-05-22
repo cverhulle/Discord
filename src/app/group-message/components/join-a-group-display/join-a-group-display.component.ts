680a4b78dad9d080c92a524c0e18fe20c60c8f09
cverhulle <corentin.verhulle@gmail.com>
2025-05-22 16:44:46 +0200

    Création d'un component pour afficher les groupes après la recherche

---
+++ src/app/group-message/components/join-a-group-display/join-a-group-display.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-join-a-group-display',
+  imports: [],
+  templateUrl: './join-a-group-display.component.html',
+  styleUrl: './join-a-group-display.component.scss'
+})
+export class JoinAGroupDisplayComponent {
+
+}
