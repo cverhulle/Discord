0a050a6791d460daea4a5148c198d70f8d32a7d6
cverhulle <corentin.verhulle@gmail.com>
2025-05-22 16:59:03 +0200

    On importe les modules nécéssaires pour le html

--- src/app/group-message/components/join-a-group-display/join-a-group-display.component.ts
+++ src/app/group-message/components/join-a-group-display/join-a-group-display.component.ts
@@ -2,17 +2,24 @@ import { Component, OnInit } from '@angular/core';
 import { JoinAGroupService } from '../../services/join-a-group.service';
 import { Observable } from 'rxjs';
 import { GroupFormInfo } from '../../models/group-info.model';
+import { SharedModule } from '../../../shared/shared.module';
+import { AsyncPipe, NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-join-a-group-display',
-  imports: [],
+  imports: [
+    SharedModule,
+    NgIf,
+    NgFor,
+    AsyncPipe
+  ],
   templateUrl: './join-a-group-display.component.html',
   styleUrl: './join-a-group-display.component.scss'
 })
 export class JoinAGroupDisplayComponent implements OnInit {
 
   // Observable pour récupérer la liste des groupes correspondant à la recherche
-  groupsList$!: Observable<GroupFormInfo[]>
+  groups$!: Observable<GroupFormInfo[]>
 
   constructor(  private joinAGroupService : JoinAGroupService) {}
 
@@ -22,6 +29,6 @@ export class JoinAGroupDisplayComponent implements OnInit {
 
   // Cette méthode permet d'initialiser l'Observable "stockant" les groupes répondant à la requete.
   private initObservables(): void{
-    this.groupsList$ = this.joinAGroupService.joinAGroup$
+    this.groups$ = this.joinAGroupService.joinAGroup$
   }
 }
