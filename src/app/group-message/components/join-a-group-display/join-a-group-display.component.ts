95ca1f4a46cda2b509bdc9f4e1469a74852e8e0d
cverhulle <corentin.verhulle@gmail.com>
2025-05-22 16:54:45 +0200

    On importe le service joinAGroup et, on crée l'Observable pour récupérer la liste des groupes après la requete

--- src/app/group-message/components/join-a-group-display/join-a-group-display.component.ts
+++ src/app/group-message/components/join-a-group-display/join-a-group-display.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { JoinAGroupService } from '../../services/join-a-group.service';
+import { Observable } from 'rxjs';
+import { GroupFormInfo } from '../../models/group-info.model';
 
 @Component({
   selector: 'app-join-a-group-display',
@@ -7,5 +10,10 @@ import { Component } from '@angular/core';
   styleUrl: './join-a-group-display.component.scss'
 })
 export class JoinAGroupDisplayComponent {
+
+  // Observable pour récupérer la liste des groupes correspondant à la recherche
+  groupsList!: Observable<GroupFormInfo[]>
+
+  constructor(  private joinAGroupService : JoinAGroupService) {}
 
 }
