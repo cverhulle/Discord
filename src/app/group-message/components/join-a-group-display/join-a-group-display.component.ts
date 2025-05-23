03de7b85756695aa3e165ed4753e5ebcafb04a6e
cverhulle <corentin.verhulle@gmail.com>
2025-05-23 16:07:55 +0200

    Ajout de la méthode onJoinGroup() sur le bouton. Implémentation de la méthode et affichage d'un message d'erreur pour les groupes privés.

--- src/app/group-message/components/join-a-group-display/join-a-group-display.component.ts
+++ src/app/group-message/components/join-a-group-display/join-a-group-display.component.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs';
 import { GroupFormInfo } from '../../models/group-info.model';
 import { SharedModule } from '../../../shared/shared.module';
 import { AsyncPipe, NgFor, NgIf } from '@angular/common';
+import { DisplayService } from '../../../shared/display/service/display.service';
 
 @Component({
   selector: 'app-join-a-group-display',
@@ -21,7 +22,8 @@ export class JoinAGroupDisplayComponent implements OnInit {
   // Observable pour récupérer la liste des groupes correspondant à la recherche
   groups$!: Observable<GroupFormInfo[]>
 
-  constructor(  private joinAGroupService : JoinAGroupService) {}
+  constructor(  private joinAGroupService : JoinAGroupService,
+                private displayService : DisplayService) {}
 
   ngOnInit(): void {
     this.initObservables()
@@ -30,5 +32,13 @@ export class JoinAGroupDisplayComponent implements OnInit {
   // Cette méthode permet d'initialiser l'Observable "stockant" les groupes répondant à la requete.
   private initObservables(): void{
     this.groups$ = this.joinAGroupService.joinAGroup$
+  }
+
+  // Cette méthode se déclenche à l'appui sur le bouton "rejoindre un groupe"
+  onJoinGroup(group : GroupFormInfo): void {
+    if (group.groupType === 'Privé') {
+      this.displayService.displayMessage("Ce groupe est privé. Vous ne pouvez pas le rejoindre.");
+      return;
+    }
   }
 }
