d94da81d84c48e00491a6298addf321de4109d6e
cverhulle <corentin.verhulle@gmail.com>
2025-05-23 16:12:22 +0200

    Début de la gestion dans le cas où le groupe est public dans onJoinGroup()

--- src/app/group-message/components/join-a-group-display/join-a-group-display.component.ts
+++ src/app/group-message/components/join-a-group-display/join-a-group-display.component.ts
@@ -36,9 +36,24 @@ export class JoinAGroupDisplayComponent implements OnInit {
 
   // Cette méthode se déclenche à l'appui sur le bouton "rejoindre un groupe"
   onJoinGroup(group : GroupFormInfo): void {
+
+    // Si le groupe est privé, on affiche un message d'erreur
     if (group.groupType === 'Privé') {
       this.displayService.displayMessage("Ce groupe est privé. Vous ne pouvez pas le rejoindre.");
       return;
     }
+
+    // Si le groupe est public
+    if (group.groupType === 'Public') {
+
+      // S'il y a déjà 10 membres, on affiche une erreur
+      if (group.members.length >= 10) {
+        this.displayService.displayMessage("Ce groupe est déjà complet (10 membres maximum).");
+        return;
+      }
+
+      // Sinon on rejoint le groupe
+    }
+
   }
 }
