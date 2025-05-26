2edf8ae529e89e0f591fa296e6757fbcfc11b238
cverhulle <corentin.verhulle@gmail.com>
2025-05-26 12:05:53 +0200

    Début de l'implémentation du .ts pour entrer le mot de passe dans le cas des groupes restreints

--- src/app/group-message/components/password-dialog/password-dialog.component.ts
+++ src/app/group-message/components/password-dialog/password-dialog.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { SharedModule } from '../../../shared/shared.module';
+import { NgModel } from '@angular/forms';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-password-dialog',
-  imports: [],
+  imports: [
+    SharedModule,
+  ],
   templateUrl: './password-dialog.component.html',
   styleUrl: './password-dialog.component.scss'
 })
 export class PasswordDialogComponent {
 
+  // Variable pour sauvegarder le mot de passe saisi
+  password : string = '';
+
+  constructor(
+    public dialogRef: MatDialogRef<PasswordDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: { groupName: string }
+  ) {}
+
+  onCancel(): void {
+    this.dialogRef.close();
+  }
+
+  onConfirm(): void {
+    this.dialogRef.close(this.password);
+  }
 }
