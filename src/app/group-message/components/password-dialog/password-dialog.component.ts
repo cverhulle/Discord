ee22c07056cdc3d3ea0988beaecb62c57a6859ef
cverhulle <corentin.verhulle@gmail.com>
2025-05-26 12:01:36 +0200

    Création d'un component pour la demande de mot de passe dans le cas des groupes Restreints

---
+++ src/app/group-message/components/password-dialog/password-dialog.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-password-dialog',
+  imports: [],
+  templateUrl: './password-dialog.component.html',
+  styleUrl: './password-dialog.component.scss'
+})
+export class PasswordDialogComponent {
+
+}
